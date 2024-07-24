a33ba07289c55485a43f4a42a7dfecdb8d42ae4a
Kaique de Miranda <kbdemiranda@hotmail.com>
2024-07-24 00:22:43 -0400

    Iniciando o Desenvolvimento

---
+++ src/main/java/io/github/fluxo/FluxoSimplesApplication.java
@@ -0,0 +1,13 @@
+package io.github.fluxo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoSimplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxoSimplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/fluxo/FluxoSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.fluxo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoSimplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
